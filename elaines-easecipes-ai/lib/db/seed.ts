8ce9e958223a11d180252ae68a1afd7dd23489e9
Elaine Huang <71571680+huang12211@users.noreply.github.com>
2026-04-17 22:19:09 -0400

    change to seed.ts to update deployed database on Railway

--- elaines-easecipes-ai/lib/db/seed.ts
+++ elaines-easecipes-ai/lib/db/seed.ts
@@ -629,7 +629,11 @@ async function seed() {
   console.log(`Seeded ${seedMeasurementUnits.length} measurement units...`)
 
   for (const recipe of seedRecipes) {
-    db.insert(recipes).values(recipe).onConflictDoNothing().run();
+    // db.insert(recipes).values(recipe).onConflictDoNothing().run();
+    db.insert(recipes).values(recipe).onConflictDoUpdate({
+      target: recipes.slug,
+      set: { image: recipe.image },
+    }).run(); 
   }
   console.log(`Seeded ${seedRecipes.length} recipes`);
 
